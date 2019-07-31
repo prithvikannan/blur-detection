68129b039ea34ad12a5673c19a9ee0196264346f
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2019-07-30 21:03:18 -0700

    added loop through directory

--- detect_blur.py
+++ detect_blur.py
@@ -1,10 +1,24 @@
 import cv2
-threshold = 110
-image = cv2.imread('images/image_005.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-score = cv2.Laplacian(image, cv2.CV_64F).var()
+import os
 
-if score > threshold:
-    print "Not Blur"
-else:
-    print "Blur"
+THRESHOLD = 110
+
+directory = 'images'
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg") or filename.endswith(".png"): 
+        path = os.path.join(directory, filename)
+        print(path)
+        image = cv2.imread(path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        score = cv2.Laplacian(image, cv2.CV_64F).var()
+        if score > THRESHOLD:
+            print "Not Blur"
+        else:
+            print "Blur"
+        continue
+    else:
+        continue
+
+
+
+    # to run: python detect_blur.py --images images
